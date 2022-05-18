27a1c13c43e63de0dc7d59d10212fb81e60fde2d
Ojasvi Soni <ojasvisoni20@gmail.com>
2022-05-18 09:08:47 +0530

    router created

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -6,6 +6,11 @@ var accountController = require("./controllers/Account");
 router.get("/send_email", accountController.sendEmail);
 
 var auth = require("./middlewares/auth");
+
 router.get("/set_profile", accountController.setProfile);
+router.use(auth.header);
+
+// routes before login
+router.put("/verify_email/:token", accountController.verify_email);
 
 module.exports = router;
