7bc41e4677870b567c9ed18c8f2f9a213b87275e
Ojasvi Soni <ojasvisoni20@gmail.com>
2022-05-24 12:24:43 +0530

    user auth created

--- api/v1/middlewares/auth.js
+++ api/v1/middlewares/auth.js
@@ -14,4 +14,29 @@ module.exports = {
 		}
 	},
 
+	user: (req, res, next) => {
+		var token = req.headers['x-user-token'];
+		if(token) {
+			jwt.verify(token, config.jwtkey, function(err, decoded){
+				if(err){
+					response.sendFail(res, "User authentication failed");
+				}else{
+
+					Users.find_by_id(decoded.id).then(function(user){
+						if(user && user.status == 'ACTIVE') {
+							req.auth = {user_id: user._id, name: user.name, email: user.email};
+							next();
+						}else{
+							response.sendFail(res, "Account is temporarily inactive. Contact Admin");
+						}
+					}).catch(function(err){
+						response.sendFail(res, "User authentication failed");
+					});
+				}
+			});
+		}else{
+			response.sendFail(res, "User authentication token required");
+		}
+	},
+
 };
