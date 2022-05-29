24723e92c833cef3bb9e295d1dd04570d9dc46f9
Ojasvi Soni <ojasvisoni20@gmail.com>
2022-05-29 14:37:18 +0530

    controller for update account information is created

--- api/v1/controllers/Profile.js
+++ api/v1/controllers/Profile.js
@@ -15,10 +15,10 @@ var Transfers = require("./../models/Transfers");
 
 
 var file_storage = multer.diskStorage({
-	destination: function(req, file, cb){
+	destination: function (req, file, cb) {
 		cb(null, "./public");
 	},
-	filename: function(req, file, cb) {
+	filename: function (req, file, cb) {
 		cb(null, file.fieldname + "-" + Date.now() + "-" + validator.escape(file.originalname));
 	}
 });
@@ -27,36 +27,51 @@ var upload = multer({
 	storage: file_storage
 });
 
-function Controller() {}
+function Controller() { }
 
 Controller.prototype.account = (req, res) => {
-	if(!req.auth.user_id) {
+	if (!req.auth.user_id) {
 		response.sendFail(res, "Unauthorised access!");
-	}else{
-		Users.account(req.auth.user_id).then(function(account){
+	} else {
+		Users.account(req.auth.user_id).then(function (account) {
 			response.sendSuccess(res, "Account Info", account);
-		}).catch(function(err){
+		}).catch(function (err) {
 			response.sendFail(res, err);
 		});
 	}
 };
 
 Controller.prototype.change_password = (req, res) => {
-	if(!req.auth.user_id) {
+	if (!req.auth.user_id) {
 		response.sendFail(res, "Unauthorised access!");
-	}else{
-		if(!req.body.old_password || !req.body.new_password) {
+	} else {
+		if (!req.body.old_password || !req.body.new_password) {
 			response.sendFail(res, "Old/New Password required");
-		}else{
-			Users.change_password({id: req.auth.user_id, new: req.body.new_password, old: req.body.old_password}, userUtil).then(function(){
+		} else {
+			Users.change_password({ id: req.auth.user_id, new: req.body.new_password, old: req.body.old_password }, userUtil).then(function () {
 				response.sendSuccess(res, "Password changed");
-			}).catch(function(err){
+			}).catch(function (err) {
 				response.sendFail(res, err);
 			});
-			
+
 		}
 	}
 };
 
+Controller.prototype.updateAccount = (req, res) => {
+	if (!req.auth.user_id) {
+		response.sendFail(res, "Unauthorised access!");
+	} else {
+		Users.update(req.auth.user_id, { name: validator.escape(req.body.name) }).then(function (updated) {
+			if (updated) {
+				response.sendSuccess(res, "Profile Updated");
+			} else {
+				response.sendFail(res, "Profile update failed");
+			}
+		}).catch(function (err) {
+			response.sendFail(res, "Profile update failed");
+		});
+	}
+};
 
 module.exports = new Controller();
